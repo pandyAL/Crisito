e71de74d4f79843a34cea1869afa551037d46f08
pandyAL <149196023+pandyAL@users.noreply.github.com>
2025-10-18 01:08:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// ----------PRIMERA VERSIÓN ----------------
+onload = () =>{
+    document.body.classList.remove("container");
+};
+//--------------------------------------------
+// ----------SEGUNDA VERSIÓN -----------------
+// document.body.classList.remove("container");
+//--------------------------------------------
+// ----------TERCERA VERSIÓN -----------------
+// document.addEventListener("DOMContentLoaded", () => {
+//    document.body.classList.remove("container");
+//});
+//--------------------------------------------
